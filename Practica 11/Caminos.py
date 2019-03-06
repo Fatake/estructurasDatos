550dbaaea93c314c15179c14b02755ae3f77b326
fatake <pace_lozano@hotmail.com>
2019-03-06 09:53:04 -0600

    Dijktra++11

--- Practica 11/Caminos.py
+++ Practica 11/Caminos.py
@@ -1,7 +1,6 @@
 import os
 from collections import deque
 
-
 ##
 #Funciones
 ##
@@ -42,13 +41,65 @@ def recorridoAnchura():
 							cola.append(str(j+1))
 							estado[j] = 0 #Listo
 	print 
-	
+
 
 def recorridoProfundidad():
 	print "\t Recorrido por Profundidad\n\n"
 
-def algoritmoDijkstra():
-	print "Por hacer" 
+def Dijkstra():
+	v = []
+	for i in range(nodos):
+		v.append(i)
+	print v
+	s = []
+	d = []
+	p = []
+	
+	aux = []
+	print "\t Algoritmo de Dijkstra\n\n"
+	while (True):
+		print "Ingrese el nodo inicial"
+		inicio = input("->")
+		if inicio <= nodos and inicio > 0:
+			break
+	inicio = inicio -1
+	
+	for i in range(nodos):
+		p.append(inicio)
+	aux = v
+	s.append(inicio)
+	##Llenar d con costos de esa fila
+	for i in range(nodos):
+		if matrizCostos[inicio][i] != 999999:
+			d.append(matrizCostos[inicio][i])
+	nodoMinimo = 50
+	print aux
+	print str(len(aux))
+	print s
+	print str(len(s))
+	print d
+	##inicio de algoritmo
+	for i in range(nodos-1):
+		for x in range(len(s)):
+			if s[x] in aux:
+				aux.remove(s[x])
+		#Busca el minimo de w 
+		for j in range(len(d)):
+			if j+1 in aux and d[j] < nodoMinimo :
+				nodoMinimo = d[j]
+		print nodoMinimo
+		#Agregar el minimo 
+		s.append(nodoMinimo)
+		for x in range(len(s)):
+			if s[x] in aux:
+				aux.remove(s[x])
+		for j in range(len(aux)):
+			if d[nodoMinimo] + matrizCostos[nodoMinimo][aux[j]] < d[aux[j]]:
+				p[aux[j]] = nodoMinimo
+				d[aux[j]] = d[nodoMinimo] + matrizCostos[nodoMinimo][aux[j]]
+	
+	print p.reverse()
+	print d.reverse()
 
 ##
 #Permite modificar la matriz de relacion
@@ -87,7 +138,7 @@ def camino():
 			recorridoProfundidad()
 		elif opcion == 3:
 			##Camino de Dijkstra
-			algoritmoDijkstra()
+			Dijkstra()
 		elif opcion == 4:
 			##Imprime Matriz
 			printMatriz()
@@ -118,6 +169,10 @@ matriz = []
 #Crea una matriz en 0
 for i in range(nodos):
 	matriz.append([0]*nodos)
+	
+matrizCostos = []
+for i in range(nodos):
+	matrizCostos.append([0]*nodos)
 
 ##
 #Preguntar por relaciones
@@ -127,8 +182,13 @@ for i in range(nodos):
 		print "Existe relacion entre el Vertice "+str(i+1)+" con "+str(j+1)
 		print "1) si y 0) no" 
 		matriz[i][j] = input("->")
-		if matriz[i][j] > 1:
+		if matriz[i][j] >= 1:
 			matriz[i][j] = 1
+			print "Ingrese el costo de la relacion\n"
+			matrizCostos[i][j] = input("->")
+		else:
+			matriz[i][j] = 0
+			matrizCostos[i][j] = 999999
 		os.system('clear')
 
 ##
